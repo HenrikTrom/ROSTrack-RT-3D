3f84bdf723d516215b126f76778c5a83619f5c4e
Henrik <training.deepl@gmail.com>
2025-11-07 10:15:59 +0100

    UPDATED install script for TUC

--- build/spinnaker/install_spinnaker.sh
+++ build/spinnaker/install_spinnaker.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 set -e
 
+# # Unpack the tarball
+# tar -xf ./spinnaker-2.4.0.143-Ubuntu20.04-amd64-pkg.tar.gz
+
+# # Run the installer
+# cp ./install_spinnaker_auto.sh ./spinnaker-2.4.0.143-amd64/
+# cd ./spinnaker-2.4.0.143-amd64
+# sh ./install_spinnaker_auto.sh
+
+# # Set environment variables
+# echo "export PATH=/opt/spinnaker/lib:\$PATH" >> /root/.bashrc
+# echo "export LD_LIBRARY_PATH=/opt/spinnaker/lib:\$LD_LIBRARY_PATH" >> /root/.bashrc
+
 # Unpack the tarball
-tar -xf ./spinnaker-2.4.0.143-Ubuntu20.04-amd64-pkg.tar.gz
+tar -xf ./spinnaker-2.5.0.80-amd64-pkg.tar.gz
 
 # Run the installer
-cp ./install_spinnaker_auto.sh ./spinnaker-2.4.0.143-amd64/
-cd ./spinnaker-2.4.0.143-amd64
+cp ./install_spinnaker_auto.sh ./spinnaker-2.5.0.80-amd64/
+cd ./spinnaker-2.5.0.80-amd64
 sh ./install_spinnaker_auto.sh
 
 # Set environment variables
